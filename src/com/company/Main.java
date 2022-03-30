37f892fc53e9ccfae05280f7854bd43b891ad8ad
Salva <salvador.lima@iesnervion.es>
2022-03-30 14:28:50 +0200

    Cositas

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,13 @@
 package com.company;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
         ArrayList<Cliente> clientes = new ArrayList<>();
+
         Cliente Lydia = new Cliente();
         Cliente Salva = new Cliente("Salva","20091571J",19);
         Cliente Marta = new Cliente("Marta","4712042L",18);
@@ -30,5 +31,42 @@ public class Main {
         System.out.println(clientes.contains(new Cliente("Salva","20091572J",19)));
 
         System.out.println(clientes.contains(new Cliente("20097421K")));
+
+        System.out.println(System.lineSeparator());
+
+        for (Cliente c: clientes) {
+            System.out.println(c);
+        }
+
+        System.out.println(System.lineSeparator());
+
+
+        Iterator<Cliente> iterator = clientes.iterator();
+
+        while (iterator.hasNext()) {
+            Cliente p =  iterator.next();
+            if (p.getEdad() == 18){
+                iterator.remove();
+            }
+        }
+
+
+        System.out.println(System.lineSeparator());
+
+
+
+        ListIterator <Cliente> listIterator = clientes.listIterator();
+
+        while (listIterator.hasNext()){
+            System.out.println(listIterator.next());
+        }
+
+        System.out.println(System.lineSeparator());
+
+        while (listIterator.hasPrevious()){
+            System.out.println(listIterator.previous());
+        }
+
+
     }
 }
